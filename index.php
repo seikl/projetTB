e5d6079069b51c6ec6dcc2b2011b34e1051e0ed8
odin <odin@VDIColIT7004.lerepuis.ch>
2014-03-06 11:13:37 +0100

    exemple accès telnet sur une borne Avaya - udate

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta charset="ISO-8859-1">
         <title>AP Manager - Accueil</title>
     </head>
     <body>
         <?php
         //Exemple d'interragation d'une borne avec la commande show system
-        $fp = fsockopen("172.16.1.29", 23, $errno, $errstr, 30);
+        $fp = fsockopen("192.168.98.36", 23, $errno, $errstr, 30);
 
         if (!$fp) {
             echo "$errstr ($errno)<br />\n";
         } else {
-            $out = "repuis\r\n";
+            $out = "abcd1234\r\n";
             $out .= "show system\r\n";
             $out .= "quit\r\n";
             fwrite($fp, $out);
@@ -26,7 +26,7 @@
 
             //file_put_contents( 'c:\temp\outputAVAYA.txt', $output );
 
-            echo utf8_decode($output);
+            echo $output;
         }
 
         ?>
