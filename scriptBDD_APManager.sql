8bc8f00b8b0a4a3e05c1bf2bf3e4891f23c50493
odin <odin@VDIColIT7004.lerepuis.ch>
2014-06-06 11:45:52 +0200

    test de requête HTTP avec modification de l'emplacement + ajout du script SQL

---
+++ scriptBDD_APManager.sql
@@ -0,0 +1,56 @@
+
+/* 
+ * Script de suppression/création automatique de la BDD pour APmanageer
+ */
+
+drop user 'apmanager'@'localhost';
+create user 'apmanager'@'localhost' identified by 'apmanager01';
+
+drop database if exists apmanagerdb;
+create database apmanagerdb;
+
+grant usage on *.* to apmanager@localhost identified by 'apmanager01';
+grant all privileges on apmanagerdb.* to apmanager@localhost ;
+
+use apmanagerdb;
+
+-- Création des tables
+CREATE TABLE IF NOT EXISTS  modeles (
+     noModeleAP INT NOT NULL AUTO_INCREMENT,
+     nomModele VARCHAR(20) NOT NULL,
+	 versionFirmware VARCHAR(8) NOT NULL,
+	 nomFabricant VARCHAR(20) NOT NULL,
+	 adrMACFabricant VARCHAR(8) NOT NULL,
+     PRIMARY KEY (noModeleAP)
+);
+
+CREATE TABLE IF NOT EXISTS  accessPoints (
+     noAP INT NOT NULL AUTO_INCREMENT,
+     nomAP VARCHAR(20) NOT NULL,
+	 adresseIPv4 VARCHAR(15) NOT NULL,
+	 username VARCHAR(20) NOT NULL,
+	 password VARCHAR(20) NOT NULL,
+	 noModeleAP INT NOT NULL REFERENCES modeles(noModeleAP),
+     PRIMARY KEY (noAP)
+);
+
+CREATE TABLE IF NOT EXISTS  lignesCommande (
+     noCli INT NOT NULL AUTO_INCREMENT,
+     ligneCommande TEXT NOT NULL,
+     protocole  SMALLINT NOT NULL,
+	 noModeleAP INT NOT NULL,
+	 noTypesCommande INT NOT NULL,
+     PRIMARY KEY (noCli,noModeleAP,noTypesCommande)
+);
+
+CREATE TABLE IF NOT EXISTS  typesCommandes (
+     noTypesCommande INT NOT NULL AUTO_INCREMENT,
+     typesCommande VARCHAR(255) NOT NULL,
+	 description VARCHAR(255),
+     PRIMARY KEY (noTypesCommande)
+);
+
+ALTER TABLE lignesCommande ADD FOREIGN KEY (noModeleAP)
+REFERENCES modeles(noModeleAP);
+ALTER TABLE lignesCommande ADD FOREIGN KEY (noTypesCommande)
+REFERENCES typesCommandes (noTypesCommande);
--- testReqHTTP.php
+++ testReqHTTP.php
@@ -6,32 +6,50 @@
     </head>
     <body>
         <?php
-        //Exemple d'interragation d'une borne avec une requête HTTP
-        $fp = fsockopen("172.16.1.29", 80, $errno, $errstr, 30);
+        
+        $uri = "EmWeb_ns%3Asnmp%3A233=APADSS0L01&EmWeb_ns%3Asnmp%3A234.0*s=testWireshark&EmWeb_ns%3Asnmp%3A235=&EmWeb_ns%3Asnmp%3A236=&EmWeb_ns%3Asnmp%3A237=";
+        
+        $adrIP ="172.16.1.29";
+        
+        //Ouverture d'un socket sur le port 80 (HTTP)
+        $fp = fsockopen($adrIP, 80, $errno, $errstr, 30);
 
         if (!$fp) {
             echo "$errstr ($errno)<br />\n";
-        } else {
-            $out = "GET /cfg/splash.html HTTP/1.1\r\n";
-            $out .= "Accept: text/html, application/xhtml+xml, */*\r\n";
-            $out .= "Host: 172.16.1.29\r\n";
-            $out .= "DNT: 1\r\n";
-            $out .= "Connection: Keep-Alive\r\n";
-            $out .= "Authorization: Basic OnJlcHVpcw==\r\n\r\n";
-            $out .= "quit\r\n";
+        } else {  
+            //requête HTTP
+            $out = "POST /cfg/system.html HTTP/1.1
+            Accept: text/html, application/xhtml+xml, */*
+            Referer: /cfg/system.html
+            Accept-Language: fr-FR
+            User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko
+            Content-Type: application/x-www-form-urlencoded
+            Accept-Encoding: gzip, deflate
+            Host: ".$adrIP."
+            Content-Length: 158
+            DNT: 1
+            Connection: Keep-Alive
+            Cache-Control: no-cache
+            Authorization: Basic OnJlcHVpcw==
 
-            fwrite($fp, $out);
+            EmWeb_ns%3Asnmp%3A233=APADSSOL01&EmWeb_ns%3Asnmp%3A234=testWireshark&EmWeb_ns%3Asnmp%3A235=&EmWeb_ns%3Asnmp%3A236.0*s=sinfi@lerepuis.ch&EmWeb_ns%3Asnmp%3A237=";
+            
+            fwrite($fp, $out);          
+            
+            $reponse = '';
 
-            $output = '';
-            while (!feof($fp)) {
-                $output .= fgets($fp, 128);
-            }
+            $reponse .= fgets($fp);
 
+            
             fclose($fp);
+            echo substr($reponse,0,500);  
+            
+            if ($reponse != '')
+                echo utf8_decode("<br>Requête envoyée avec succès.");
+            else
+                echo utf8_decode("<br>Erreur dans la commmande."); 
 
             //file_put_contents( 'c:\temp\outputAVAYA.html', $output );
-
-            echo substr($output,0,20);
         }
 
         ?>
