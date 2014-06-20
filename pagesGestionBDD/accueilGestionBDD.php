f688e2be0da1d54a92bfc208d17ffed5d359fe43
odin <odin@VDIColIT7004.lerepuis.ch>
2014-06-20 12:31:22 +0200

    temnplate temriné + pages accueil BDD et AP

--- pagesGestionBDD/accueilGestionBDD.php
+++ pagesGestionBDD/accueilGestionBDD.php
@@ -23,8 +23,8 @@
                  </td>                 
                  <td>                           
                     <ul class="nav nav-tabs nav-justified">
-                     <li class="active"><a href="../pagesGestionAP/accueilGestionAP.php">Gestion des AP</a></li>
-                     <li><a href="../pagesGestionBDD/accueilGestionBDD.php">Gestion des enregistrements de la BDD</a></li>
+                     <li><a href="../pagesGestionAP/accueilGestionAP.php">Gestion des AP</a></li>
+                     <li class="active"><a href="../pagesGestionBDD/accueilGestionBDD.php">Gestion des enregistrements de la BDD</a></li>
                      <li><a href="#">Configuration syst&egrave;me</a></li>
                     </ul>
                     <br>           
@@ -32,15 +32,24 @@
               </tr>
               <tr>
                  <td width="30%" class="leftmenu">
-                        <p><b>Informations sur les AP</b></p>
-                        <ul class="nav nav-pills nav-stacked">                       
-                           <li><a href="#">Afficher la liste  de tous les AP inscrits</a></li>
-                           <li><a href="#">Interroger un AP</a></li>                       
+                        <p><b>G&eacute;rer les enregistrments des AP</b></p>
+                        <ul class="nav nav-pills nav-justified">                       
+                           <li><a href="#">Ajouter</a></li>
+                           <li><a href="#">Modifier</a></li>                       
+                           <li><a href="#">Supprimer</a></li>
                         </ul>
-                        <p><b>Configurer les AP</b></p>
-                        <ul class="nav nav-pills nav-stacked">                       
-                           <li><a href="#">Appliquer une commande &agrave; un ou plusieurs AP</a></li>    
-                        </ul> 
+                         <p><b>G&eacute;rer les mod&egrave;les enregistr&eacute;s</b></p>
+                        <ul class="nav nav-pills nav-justified">                       
+                           <li><a href="#">Ajouter</a></li>
+                           <li><a href="#">Modifier</a></li>                       
+                           <li><a href="#">Supprimer</a></li>
+                        </ul>
+                         <p><b>G&eacute;rer les lignes de commandes (CLI)</b></p>
+                        <ul class="nav nav-pills nav-justified">                       
+                           <li><a href="#">Ajouter</a></li>
+                           <li><a href="#">Modifier</a></li>                       
+                           <li><a href="#">Supprimer</a></li>
+                        </ul>                      
                  </td>  
                  <td width="2px">                       
                       &nbsp;
@@ -48,7 +57,7 @@
                  <td class="informations">
                      
                      <ol class="breadcrumb">
-                        <li><a href="#">Gestion des AP</a></li>
+                        <li><a href="#">Gestion de la BDD</a></li>
                         <li class="active">Accueil</li>
                     </ol>
                      INFOS
