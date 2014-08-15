6394d6bb43250f1ec34590bc7d2cc57fd3f7f3bc
odin <odin@VDIColIT7004.lerepuis.ch>
2014-08-15 11:49:05 +0200

    - ajout des requêtes HTTP  (applqiuerCommande.php) - retrait des balises HTML dans les réponses HTTP - ajout d'une imprimante dans le script SQL - Ajout flush ARP (dû à limite de 1024 sur centOS)

--- includes/scanIP.php
+++ includes/scanIP.php
@@ -3,14 +3,21 @@
     set_time_limit(300);
     
     //scan du réseau avec l'adresse de début, de fin et la MAC à rechercher (source originale: https://github.com/Harvie/Programs/blob/master/php/mac_hack.phps, le 10.09.2014
-    function quick_ipmac_scan($adrDebutLong, $adrFinLong) {       
+    function quick_ipmac_scan($adrDebutLong, $adrFinLong) {  
+        
+        $getArpWindows= "arp -a";//pour Windows
+        $getArpLinux= "arp -vn";//pour Linux
+
+        $flushArp="ip -s -s neigh flush all";//pour Linux
+
+        $pingLinux="ping -c 1 -W 1 ";//Pour Linux
+        $pingWindows="ping -n 1 -w 1 ";//Pour windows  
+        $arp=null;
         
       for($i=$adrDebutLong;$i<=$adrFinLong;$i++) {
         //Mega threaded ( This will open 255 processes ;))
         $ipAPinger=long2ip($i);
-        //$fp[$i] = popen("ping -c 1 -W 1 ".$ipAPinger, "r");//Pour Linux
-        $fp[$i] = popen("ping -n 1 -w 1 ".$ipAPinger, "r");//Pour windows
-        //echo "<br>pour INFO, IP A PINGER: ".$ipAPinger. "  --- etat de 'i': ".$i;        
+        $fp[$i] = popen($pingWindows.$ipAPinger, "r");       
         
         //pour éviter d'atteindre la limite de processus ouverts
         if ((($i % 16) == 0) && (($adrFinLong - $adrDebutLong) >= 512)){
@@ -18,17 +25,19 @@
                     pclose($fp[$j]);
             }
         }   
-        //pclose($fp[$i]);
+        //pour vider la table ARP du serveur
+        if (($i % 256)==0){
+            $arp .= shell_exec($getArpWindows);
+            shell_exec($flushArp);
+        }
       }
-      
+     
       /*
       for($i=$adrDebutLong;$i<=$adrFinLong;$i++) {
         while( $fp[$i] && !feof($fp[$i]) ) { fgets($fp[$i]); }
       } 
       */
-      
-      //$arp = shell_exec("arp -vn"); //pour Linux
-      $arp = shell_exec("arp -a"); //pour Windows            
+      $arp .= shell_exec($getArpWindows);        
       $tableARP= explode("\n", $arp);
        
       //print_r($tableARP); //DEBUG
